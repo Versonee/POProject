a0557b91ba3d71dbbe127187133a3e57e0945039
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2019-06-16 16:37:39 +0200

    usprawnienie Trasy i biletów

--- src/Bilet.java
+++ src/Bilet.java
@@ -1,2 +1,21 @@
 public class Bilet {
+    private int  numer_miejsca;
+    private int numer_lotu;
+
+    public Bilet(int nr_lotu, int nr_miejsca)
+    {
+        this.numer_lotu = nr_lotu;
+        this.numer_miejsca = nr_miejsca;
+    }
+
+    public int NumerMiejsca()
+    {
+        return numer_miejsca;
+    }
+
+    public int NumerLotu()
+    {
+        return numer_lotu;
+    }
+
 }
--- src/Trasa.java
+++ src/Trasa.java
@@ -3,44 +3,68 @@ import java.util.List;
 
 
 public class Trasa implements Serializable {
-    public Lotnisko start;
-    public Lotnisko koniec;
-    public double odleglosc;
-    public Samolot samolot;
-    public String data;
-    public int miejsca[];
+    private Lotnisko start;
+    private Lotnisko koniec;
+    private double odleglosc;
+    private Samolot samolot;
+    private String data;
+    private int numer_lotu;
+    private int miejsca[];
+    private List<Klient> pasazerowie;
 
-    public Trasa(Lotnisko start, Lotnisko koniec, double odleglosc,Samolot samolot, String data){
+    public Trasa(Lotnisko start, Lotnisko koniec, double odleglosc,Samolot samolot, String data, int numer_lotu){
         this.start = start;
         this.koniec = koniec;
         this.samolot = samolot;
         this.data = data;
         this.odleglosc = odleglosc;
+        this.numer_lotu = numer_lotu;
         miejsca = new int[samolot.l_miejsc];
         for(int i = 0; i < miejsca.length; i++) //gdy miejsce od i ==0 znaczy że wolne jak 1 znaczy że zajęte
             miejsca[i] = 0;
 
     }
 
-    //dokoncze
-    public boolean RezerwojMiejsce(int nr)
+    public String Data()
     {
-        if(miejsca[nr] == 0 )
-        {
-            miejsca[nr] = 1;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return data;
     }
-    //dokonczycze
+
+    public int NumberLotu()
+    {
+        return numer_lotu;
+    }
+
+    //zwraca true jak sie udało zareerwowac wszystkie miejsca lub fail jak nie
+    public boolean RezerwojMiejsceOsoba(Klient klient, int[]miejsca)
+    {
+        int tab[] = new int[10];
+        for(int i = 0; i < miejsca.length; i++)
+        {
+            if(this.miejsca[miejsca[i]] != 0)
+            {
+                return false;
+            }
+        }
+
+        for(int i = 0; i < miejsca.length; i++)
+        {
+            Bilet bilet = new Bilet(numer_lotu, miejsca[i]);
+            this.miejsca[miejsca[i]] = 1;
+            klient.bilety().add(bilet);
+        }
+        pasazerowie.add(klient);
+        return true;
+    }
+
+    //dokoncze
     public void ZwolnijMiejsce(int nr)
     {
 
     }
 
-
-
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
