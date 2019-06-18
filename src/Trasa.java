d8b5354ed9369a26565d364b5156016f07ef9347
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2019-06-18 19:15:50 +0200

    dadanie funkcji update do main

--- src/Trasa.java
+++ src/Trasa.java
@@ -1,5 +1,6 @@
 import java.awt.image.SampleModel;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -8,15 +9,17 @@ public class Trasa implements Serializable {
     private Lotnisko start;
     private Lotnisko koniec;
     private double odleglosc;
+    private LocalDate data_utworzenia;
     private Samolot samolot;
     private int Co_ile;
 
-    public Trasa(Lotnisko start, Lotnisko koniec, double odleglosc,Samolot samolot, int co_ile){
+    public Trasa(Lotnisko start, Lotnisko koniec, double odleglosc,Samolot samolot, int co_ile, LocalDate data){
         this.start = start;
         this.koniec = koniec;
         this.samolot = samolot;
         this.odleglosc = odleglosc;
         this.Co_ile = co_ile;
+        this.data_utworzenia = data;
     }
     //gettery
     public Lotnisko Start() { return start; }
@@ -33,9 +36,11 @@ public class Trasa implements Serializable {
 
     public int CoIle() { return Co_ile; }
 
+    public LocalDate DataUtworzenia() { return data_utworzenia;}
+
     //funkcja zwracajaca nowo utworzony lot
     //dokoncze
-    public Lot NowyLot(String data, int numer)
+    public Lot NowyLot(LocalDate data, int numer)
     {
 
         Lot lot = new Lot(this, data, numer);
